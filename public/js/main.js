053933ffd883a503c017bbb317f4708fe9608387
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-02-09 22:46:52 +0100

    feat: rtl/ltr buttons

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+document
+  .getElementById("btn-ltr")
+  .addEventListener("click", function () {
+    document.documentElement.dir = "ltr";
+  });
+document
+  .getElementById("btn-rtl")
+  .addEventListener("click", function () {
+    document.documentElement.dir = "rtl";
+  });
