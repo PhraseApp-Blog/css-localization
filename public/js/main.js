5994bf80357c6159f1d7a98004a3acde40d6968f
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-02-10 15:56:57 +0100

    feat: logical sizing

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,20 @@ document.querySelectorAll(".btn-rtl").forEach((btn) => {
     document.documentElement.dir = "rtl";
   });
 });
+
+document
+  .querySelectorAll(".btn-writing-mode")
+  .forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const targetSelector =
+        btn.getAttribute("data-target");
+      const targets =
+        document.querySelectorAll(targetSelector);
+
+      const writingMode = btn.getAttribute("data-value");
+
+      targets.forEach((target) => {
+        target.style.writingMode = writingMode;
+      });
+    });
+  });
